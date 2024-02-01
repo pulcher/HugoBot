59173f7812d3d15f8e443875a49501f5fc24e0de
Harold Pulcher <pulcher@pulcher.biz>
2024-01-31 22:44:47 -0600

    Added in the base menu and the games menu seems to work

--- HugoBot-A/HugoBot-A.ino
+++ HugoBot-A/HugoBot-A.ino
@@ -41,8 +41,8 @@
 #include "Adafruit_ST7735.h"
 #include "Adafruit_ST7735_Menu.h"
 #include "Colors.h"
-#include <Adafruit_seesaw.h>
-#include <Encoder.h>
+#include "Adafruit_seesaw.h"
+
 // found in \Arduino\libraries\Adafruit-GFX-Library-master
 #include "fonts\FreeSans9pt7b.h"
 #include "fonts\FreeSans12pt7b.h"
@@ -67,14 +67,7 @@
 #define TFT_MOSI 12  // Data out
 #define TFT_SCLK 13  // Clock out
 
-#define EN1_PIN 4
-#define EN2_PIN 5
-#define SE_PIN 6
-
 // easy way to include fonts but change globally
-// #define FONT_MICRO 1  // font for menus
-// #define FONT_SMALL 2  // font for menus
-// #define FONT_TITLE 3  // font for menus
 #define FONT_SMALL FreeSans9pt7b      // font for menus
 #define FONT_EDITTITLE FreeSans9pt7b  // font for menus
 #define FONT_ITEM FreeSans9pt7b       // font for menus
@@ -107,7 +100,9 @@ int AllowColorMenu = 0;
 
 // must have variables for each menu item
 // best to have these global so you can use them in processing functions
-int MenuOption1 = 0, MenuOption2 = 0, MenuOption3 = 0;
+int MenuOption1 = 0, MenuOption2 = 0, MenuOption3 = 0, MenuOption4 = 0, MenuOption5 = 0;
+int InformationOptions1 = 0, InformationOptions2 = 0;
+int GamesOptions1 = 0, GamesOptions2 = 0, GamesOptions3 = 0;
 
 int OptionOption1 = 0, OptionOption2 = 0, OptionOption3 = 0;
 
@@ -129,7 +124,6 @@ const char *OffOnItems[] = { "Off", "On" };
 
 const char *DataRateItems[] = { "300b", "1.2kb", "2.4kb", "4.8kb", "9.6kb", "19.2kb", "56kb" };
 
-// you know the drill
 Adafruit_ST7735 Display = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 // fire up the seesaw interface.
@@ -138,6 +132,11 @@ Adafruit_seesaw ss(&Wire1);
 // required, you must create either an Item menu (no inline editing) or an EditMenu (allows inline editing)
 //ClassName YourMenuName(&DisplayObject, True=Touch input, False(Default)=mechanical input);
 ItemMenu MainMenu(&Display);
+EditMenu InformationMenu(&Display);
+EditMenu GamesMenu(&Display);
+EditMenu CalibrationMenu(&Display);
+EditMenu UtilitiesMenu(&Display);
+EditMenu SettingsMenu(&Display);
 
 // since we're showing both menu types, create an object for each where the item menu is the main and calls edit menus
 // you can have an item menu call other item menus an edit menu can call an edit menu but in a round about way--not recommended
@@ -146,164 +145,108 @@ EditMenu OptionMenu(&Display);    // default is false, need not specify
 EditMenu WirelessMenu(&Display);  // or you can still call false to force mechanical input selection
 EditMenu ServoMenu(&Display);
 
-//Encoder encoder(EN1_PIN, EN2_PIN);
-
-void setup() {
-
-  Serial.begin(115200);
-
-  // button in the encoder
-  pinMode(SE_PIN, INPUT_PULLUP);
-
-  // fire up the display
-  Display.initR(INITR_GREENTAB);
-  Display.setRotation(3);
-
-  // Initialize the SeeSaw
-  if(!ss.begin(SEESAW_ADDR)) {
-    Serial.println(F("Unable to find SeeSaw"));
-    while(1);
-  }
-
-  Serial.println("Found Product 5740");
-
-  ss.pinMode(SS_SWITCH_UP, INPUT_PULLUP);
-  ss.pinMode(SS_SWITCH_DOWN, INPUT_PULLUP);
-  ss.pinMode(SS_SWITCH_LEFT, INPUT_PULLUP);
-  ss.pinMode(SS_SWITCH_RIGHT, INPUT_PULLUP);
-  ss.pinMode(SS_SWITCH_SELECT, INPUT_PULLUP);
-
-  // get starting position
-  Position = ss.getEncoderPosition();
-
-  Serial.println("Turning on interrupts");
-  ss.enableEncoderInterrupt();
-  ss.setGPIOInterrupts((uint32_t)1 << SS_SWITCH_UP, 1);
-
-  MainMenu.init(MENU_TEXT, MENU_BACKGROUND, MENU_HIGHLIGHTTEXT, MENU_HIGHLIGHT, 20, 3, "Main", FONT_SMALL, FONT_TITLE);
-
-  MenuOption1 = MainMenu.addNI("Options");
-  MenuOption2 = MainMenu.addNI("Wireless");
-  MenuOption3 = MainMenu.addNI("Servos");
-
-  MainMenu.setTitleColors(TITLE_TEXT, TITLE_BACK);
-  MainMenu.setTitleBarSize(0, 0, 160, 20);
-  MainMenu.setTitleTextMargins(10, 15);
-  MainMenu.setMenuBarMargins(0, 160, 2, 1);
-  MainMenu.setItemTextMargins(10, 15, 5);
-  MainMenu.setItemColors(C_GREY, MENU_SELECTBORDER);
-
-  OptionMenu.init(MENU_TEXT, MENU_BACKGROUND, MENU_HIGHLIGHTTEXT, MENU_HIGHLIGHT, MENU_SELECTTEXT, MENU_SELECT,
-                  DATA_COLUMN, 30, 3, "Options", FONT_SMALL, FONT_TITLE);
-
-  int addNI(const char *ItemText, float Data, float LowLimit, float HighLimit,
-            float Increment, byte DecimalPlaces = 0, const char **ItemMenuText = NULL);
-
-  OptionOption1 = OptionMenu.add565("Temp.", Temp2Adj, -1.0, 1.0, .05, 2, NULL);
-  OptionOption2 = OptionMenu.add565("Read", 2, 0, sizeof(ReadoutItems) / sizeof(ReadoutItems[0]), 1, 0, ReadoutItems);
-  OptionOption3 = OptionMenu.add565("Tune", 0, 0, 20, 1, 0, NULL);
-
-  OptionMenu.SetItemValue(OptionOption1, 0.12);
-  OptionMenu.SetItemValue(OptionOption3, 1);
-  OptionMenu.setTitleColors(TITLE_TEXT, TITLE_BACK);
-  OptionMenu.setTitleBarSize(0, 0, 160, 30);
-  OptionMenu.setTitleTextMargins(10, 5);
-  OptionMenu.setItemTextMargins(10, 10, 5);
-  OptionMenu.setMenuBarMargins(0, 160, 2, 1);
-  OptionMenu.setItemColors(C_GREY, MENU_SELECTBORDER, MENU_HIGHBORDER);
-
-  WirelessMenu.init(MENU_TEXT, MENU_BACKGROUND, MENU_HIGHLIGHTTEXT, MENU_HIGHLIGHT, MENU_SELECTTEXT, MENU_SELECT,
-                    DATA_COLUMN, 30, 3, "Wireless Menu", FONT_SMALL, FONT_TITLE);
-  WirelessOption1 = WirelessMenu.addNI("Address", 0, 0, 255, 1, 0);
-  WirelessOption2 = WirelessMenu.addNI("Data", 2, 0, sizeof(DataRateItems) / sizeof(DataRateItems[0]), 1, 0, DataRateItems);
-  WirelessOption3 = WirelessMenu.addNI("Air", 2, 0, sizeof(DataRateItems) / sizeof(DataRateItems[0]), 1, 0, DataRateItems);
-  WirelessOption4 = WirelessMenu.addNI("Error", 1, 0, sizeof(OffOnItems) / sizeof(OffOnItems[0]), 1, 0, OffOnItems);
-
-  WirelessMenu.setTitleBarSize(0, 0, 160, 30);
-  WirelessMenu.setTitleTextMargins(10, 5);
-  WirelessMenu.setItemTextMargins(10, 10, 5);
-  WirelessMenu.setMenuBarMargins(0, 160, 2, 1);
-  WirelessMenu.setTitleColors(TITLE_TEXT, TITLE_BACK);
-  WirelessMenu.setItemColors(C_GREY, MENU_SELECTBORDER, MENU_HIGHBORDER);
-
-  ServoMenu.init(MENU_TEXT, MENU_BACKGROUND, MENU_HIGHLIGHTTEXT, MENU_HIGHLIGHT, MENU_SELECTTEXT, MENU_SELECT,
-                 DATA_COLUMN, 20, 5, "Servos", FONT_SMALL, FONT_SMALL);
-  ServoMenu1 = ServoMenu.addNI("Address", 0, 0, 255, 1, 0);
-  ServoMenu2 = ServoMenu.addNI("Prec", 2, 0, sizeof(PrecisionItems) / sizeof(PrecisionItems[0]), 1, 0, PrecisionItems);
-  ServoMenu3 = ServoMenu.addNI("Tune", 2, 0, sizeof(TuneItems) / sizeof(TuneItems[0]), 1, 0, TuneItems);
-  ServoMenu4 = ServoMenu.addNI("State", 1, 0, sizeof(OffOnItems) / sizeof(OffOnItems[0]), 1, 0, OffOnItems);
-  ServoMenu5 = ServoMenu.addNI("Tune", 2, 0, sizeof(TuneItems) / sizeof(TuneItems[0]), 1, 0, TuneItems);
-  ServoMenu6 = ServoMenu.addNI("State", 1, 0, sizeof(OffOnItems) / sizeof(OffOnItems[0]), 1, 0, OffOnItems);
-
-  ServoMenu.setTitleBarSize(0, 0, 160, 20);
-  ServoMenu.setTitleTextMargins(10, 15);
-  ServoMenu.setItemTextMargins(10, 15, 10);
-  ServoMenu.setMenuBarMargins(0, 160, 2, 1);
-  ServoMenu.setTitleColors(TITLE_TEXT, TITLE_BACK);
-  ServoMenu.setItemColors(C_GREY, MENU_SELECTBORDER, MENU_HIGHBORDER);
-
-  ProcessMainMenu();
-
-  // menu code done, now proceed to your code
-  Display.fillScreen(MENU_BACKGROUND);
-}
-
-void loop() {
-}
 
 // function to process main menu iteraction
-// ideally this implementation makes it easy to launch your menu from anywhere in th
+
+
+void ProcessInformationMenu() {
+}
+
+void ProcessGamesMenu() {
+  int EditGamesOption = 1;
+
+  Display.fillScreen(MENU_BACKGROUND);
+  GamesMenu.draw();
+
+  while (EditGamesOption != 0) {
+
+    // standard encoder read
+    Position = ss.getEncoderPosition();
+    delay(DEBOUNCE);
+    
+    if ((Position - oldPosition) > 0) {
+      while (oldPosition != Position) {
+        oldPosition = Position;
+        Position = ss.getEncoderPosition();
+        delay(DEBOUNCE);
+      }
+
+      GamesMenu.MoveUp();
+    }
+
+    if ((Position - oldPosition) < 0) {
+
+      while (oldPosition != Position) {
+        oldPosition = Position;
+        Position = ss.getEncoderPosition();\
+        delay(DEBOUNCE);
+      }
+
+      GamesMenu.MoveDown();
+    }
+
+    if (!ss.digitalRead(SS_SWITCH_SELECT)) {
+      while (!ss.digitalRead(SS_SWITCH_SELECT)) {
+        delay(DEBOUNCE);
+      }
+
+      EditGamesOption = GamesMenu.selectRow();
+    }
+  }
+}
+
+void ProcessCalibrationMenu() {
+
+}
+
+void ProcessUtilitiesMenu() {
+
+}
+
+void ProcessSettingsMenu() {
+
+}
+
 void ProcessMainMenu() {
 
   // set an inital flag that will be used to store what menu item the user exited on
   int MainMenuOption = 1;
 
-  // blank out the screen
   Display.fillScreen(MENU_BACKGROUND);
-
-  // draw the main menu
   MainMenu.draw();
 
   // run the processing loop until user move selector to title bar (which becomes exit, i.e. 0 return val)
   // and selectes it
   // note menu code can return - 1 for errors so run unitl non zero
-
   while (MainMenuOption != 0) {
 
-    // standard encoder read
-    Position = ss.getEncoderPosition(); //encoder.read();
-    delay(DEBOUNCE);
-    // attempt to debouce these darn things...
-    if ((Position - oldPosition) > 0) {
+    //getEncoder(&MainMenu); 
 
+    Position = ss.getEncoderPosition();
+    delay(DEBOUNCE);
+    
+    if ((Position - oldPosition) > 0) {
       while (oldPosition != Position) {
         oldPosition = Position;
-        Position = ss.getEncoderPosition(); //encoder.read();
+        Position = ss.getEncoderPosition();
         delay(DEBOUNCE);
       }
-
-      // once encoder calms down and is done cycling, move selector up
-      // since encoder reads are increasing
-      // any menu wrapping is handled in the library
       MainMenu.MoveUp();
     }
-    // attempt to debouce these darn things...
+
     if ((Position - oldPosition) < 0) {
 
       while (oldPosition != Position) {
         oldPosition = Position;
-        Position = ss.getEncoderPosition(); //encoder.read();
+        Position = ss.getEncoderPosition();
         delay(DEBOUNCE);
       }
-      // once encoder calms down and is done cycling, move selector up
-      // since encoder reads are decreasing
-      // any menu wrapping is handled in the library
+
       MainMenu.MoveDown();
     }
 
     // but wait...the user pressed the button on the encoder
-    //if (digitalRead(SE_PIN) == LOW) {
-      if (!ss.digitalRead(SS_SWITCH_SELECT)) {
+    if (!ss.digitalRead(SS_SWITCH_SELECT)) {
 
       // debounce the button press
       while (!ss.digitalRead(SS_SWITCH_SELECT)) {
@@ -315,31 +258,38 @@ void ProcessMainMenu() {
 
       // here is where you process accordingly
       // remember on pressing button on title bar 0 is returned and will exit the loop
-
       if (MainMenuOption == MenuOption1) {
-        // item 1 was the Option menu
-        ProcessOptionMenu();
-        // when done processing that menu, return here
-        // clear display and redraw this main menu
+        ProcessInformationMenu();
+
         Display.fillScreen(MENU_BACKGROUND);
         MainMenu.draw();
       }
 
       if (MainMenuOption == MenuOption2) {
-        ProcessWirelessMenu();
+        ProcessGamesMenu();
         Display.fillScreen(MENU_BACKGROUND);
         MainMenu.draw();
       }
 
       if (MainMenuOption == MenuOption3) {
-        ProcessServoMenu();
+        ProcessCalibrationMenu();
         Display.fillScreen(MENU_BACKGROUND);
         MainMenu.draw();
       }
+
+      if (MainMenuOption == MenuOption4) {
+        ProcessUtilitiesMenu();
+        Display.fillScreen(MENU_BACKGROUND);
+        MainMenu.draw();
+      }
+
+      if (MainMenuOption == MenuOption5) {
+        ProcessSettingsMenu();
+        Display.fillScreen(MENU_BACKGROUND);
+        MainMenu.draw();
+      }      
     }
   }
-
-  // at this point MenuOption better be 0...
 }
 
 // menu to handle processing for a sub-menu
@@ -501,7 +451,121 @@ void ProcessServoMenu() {
 }
 
 
+void setup() {
 
+  Serial.begin(115200);
+
+  // fire up the display
+  Display.initR(INITR_GREENTAB);
+  Display.setRotation(3);
+
+  // Initialize the SeeSaw
+  if(!ss.begin(SEESAW_ADDR)) {
+    Serial.println(F("Unable to find SeeSaw"));
+    while(1);
+  }
+
+  Serial.println("Found Product 5740");
+
+  ss.pinMode(SS_SWITCH_UP, INPUT_PULLUP);
+  ss.pinMode(SS_SWITCH_DOWN, INPUT_PULLUP);
+  ss.pinMode(SS_SWITCH_LEFT, INPUT_PULLUP);
+  ss.pinMode(SS_SWITCH_RIGHT, INPUT_PULLUP);
+  ss.pinMode(SS_SWITCH_SELECT, INPUT_PULLUP);
+
+  // get starting position
+  Position = ss.getEncoderPosition();
+
+  Serial.println("Turning on interrupts");
+  ss.enableEncoderInterrupt();
+  ss.setGPIOInterrupts((uint32_t)1 << SS_SWITCH_UP, 1);
+
+  MainMenu.init(MENU_TEXT, MENU_BACKGROUND, MENU_HIGHLIGHTTEXT, MENU_HIGHLIGHT, 20, 5, "Main", FONT_SMALL, FONT_TITLE);
+
+  MenuOption1 = MainMenu.addNI("Information");
+  MenuOption2 = MainMenu.addNI("Games");
+  MenuOption3 = MainMenu.addNI("Calibration");
+  MenuOption4 = MainMenu.addNI("Utilities");
+  MenuOption5 = MainMenu.addNI("Settings");
+
+  MainMenu.setTitleColors(TITLE_TEXT, TITLE_BACK);
+  MainMenu.setTitleBarSize(0, 0, 160, 20);
+  MainMenu.setTitleTextMargins(10, 15);
+  MainMenu.setMenuBarMargins(0, 160, 2, 1);
+  MainMenu.setItemTextMargins(10, 15, 5);
+  MainMenu.setItemColors(C_GREY, MENU_SELECTBORDER);
+
+  int addNI(const char *ItemText, float Data, float LowLimit, float HighLimit,
+            float Increment, byte DecimalPlaces = 0, const char **ItemMenuText = NULL);
+
+  GamesMenu.init(MENU_TEXT, MENU_BACKGROUND, MENU_HIGHLIGHTTEXT, MENU_HIGHLIGHT, MENU_SELECTTEXT, MENU_SELECT,
+                DATA_COLUMN, 20, 5, "Games", FONT_SMALL, FONT_SMALL);
+
+  GamesOptions1 = GamesMenu.addNI("Six", 1, 0, sizeof(OffOnItems) / sizeof(OffOnItems[0]), 1, 0, OffOnItems);
+  GamesOptions2 = GamesMenu.addNI("4", 1, 0, sizeof(OffOnItems) / sizeof(OffOnItems[0]), 1, 0, OffOnItems);  
+  GamesOptions3 = GamesMenu.addNI("Quick", 1, 0, sizeof(OffOnItems) / sizeof(OffOnItems[0]), 1, 0, OffOnItems);              
+
+  GamesMenu.setTitleColors(TITLE_TEXT, TITLE_BACK);
+  GamesMenu.setTitleBarSize(0, 0, 160, 20);
+  GamesMenu.setTitleTextMargins(5, 10);
+  GamesMenu.setMenuBarMargins(0, 160, 2, 1);
+  GamesMenu.setItemTextMargins(5, 15, 0);
+  GamesMenu.setItemColors(C_GREY, MENU_SELECTBORDER, MENU_HIGHBORDER);
+
+  OptionMenu.init(MENU_TEXT, MENU_BACKGROUND, MENU_HIGHLIGHTTEXT, MENU_HIGHLIGHT, MENU_SELECTTEXT, MENU_SELECT,
+                  DATA_COLUMN, 30, 3, "Options", FONT_SMALL, FONT_TITLE);
+
+  OptionOption1 = OptionMenu.add565("Temp.", Temp2Adj, -1.0, 1.0, .05, 2, NULL);
+  OptionOption2 = OptionMenu.add565("Read", 2, 0, sizeof(ReadoutItems) / sizeof(ReadoutItems[0]), 1, 0, ReadoutItems);
+  OptionOption3 = OptionMenu.add565("Tune", 0, 0, 20, 1, 0, NULL);
+
+  OptionMenu.SetItemValue(OptionOption1, 0.12);
+  OptionMenu.SetItemValue(OptionOption3, 1);
+  OptionMenu.setTitleColors(TITLE_TEXT, TITLE_BACK);
+  OptionMenu.setTitleBarSize(0, 0, 160, 30);
+  OptionMenu.setTitleTextMargins(10, 5);
+  OptionMenu.setItemTextMargins(10, 10, 5);
+  OptionMenu.setMenuBarMargins(0, 160, 2, 1);
+  OptionMenu.setItemColors(C_GREY, MENU_SELECTBORDER, MENU_HIGHBORDER);
+
+  WirelessMenu.init(MENU_TEXT, MENU_BACKGROUND, MENU_HIGHLIGHTTEXT, MENU_HIGHLIGHT, MENU_SELECTTEXT, MENU_SELECT,
+                    DATA_COLUMN, 30, 3, "Wireless Menu", FONT_SMALL, FONT_TITLE);
+  WirelessOption1 = WirelessMenu.addNI("Address", 0, 0, 255, 1, 0);
+  WirelessOption2 = WirelessMenu.addNI("Data", 2, 0, sizeof(DataRateItems) / sizeof(DataRateItems[0]), 1, 0, DataRateItems);
+  WirelessOption3 = WirelessMenu.addNI("Air", 2, 0, sizeof(DataRateItems) / sizeof(DataRateItems[0]), 1, 0, DataRateItems);
+  WirelessOption4 = WirelessMenu.addNI("Error", 1, 0, sizeof(OffOnItems) / sizeof(OffOnItems[0]), 1, 0, OffOnItems);
+
+  WirelessMenu.setTitleBarSize(0, 0, 160, 30);
+  WirelessMenu.setTitleTextMargins(10, 5);
+  WirelessMenu.setItemTextMargins(10, 10, 5);
+  WirelessMenu.setMenuBarMargins(0, 160, 2, 1);
+  WirelessMenu.setTitleColors(TITLE_TEXT, TITLE_BACK);
+  WirelessMenu.setItemColors(C_GREY, MENU_SELECTBORDER, MENU_HIGHBORDER);
+
+  ServoMenu.init(MENU_TEXT, MENU_BACKGROUND, MENU_HIGHLIGHTTEXT, MENU_HIGHLIGHT, MENU_SELECTTEXT, MENU_SELECT,
+                 DATA_COLUMN, 20, 5, "Servos", FONT_SMALL, FONT_SMALL);
+  ServoMenu1 = ServoMenu.addNI("Address", 0, 0, 255, 1, 0);
+  ServoMenu2 = ServoMenu.addNI("Prec", 2, 0, sizeof(PrecisionItems) / sizeof(PrecisionItems[0]), 1, 0, PrecisionItems);
+  ServoMenu3 = ServoMenu.addNI("Tune", 2, 0, sizeof(TuneItems) / sizeof(TuneItems[0]), 1, 0, TuneItems);
+  ServoMenu4 = ServoMenu.addNI("State", 1, 0, sizeof(OffOnItems) / sizeof(OffOnItems[0]), 1, 0, OffOnItems);
+  ServoMenu5 = ServoMenu.addNI("Tune", 2, 0, sizeof(TuneItems) / sizeof(TuneItems[0]), 1, 0, TuneItems);
+  ServoMenu6 = ServoMenu.addNI("State", 1, 0, sizeof(OffOnItems) / sizeof(OffOnItems[0]), 1, 0, OffOnItems);
+
+  ServoMenu.setTitleBarSize(0, 0, 160, 20);
+  ServoMenu.setTitleTextMargins(10, 15);
+  ServoMenu.setItemTextMargins(10, 15, 10);
+  ServoMenu.setMenuBarMargins(0, 160, 2, 1);
+  ServoMenu.setTitleColors(TITLE_TEXT, TITLE_BACK);
+  ServoMenu.setItemColors(C_GREY, MENU_SELECTBORDER, MENU_HIGHBORDER);
+
+  ProcessMainMenu();
+
+  // menu code done, now proceed to your code
+  Display.fillScreen(MENU_BACKGROUND);
+}
+
+void loop() {
+}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // end of example
 //////////////////////////////////////////////////////////////////////////////////////////////////
